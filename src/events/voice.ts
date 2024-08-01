db2235a82f3c3983901f6a931b2fe812c819d1ac
JustEvil <thatjustevil@gmail.com>
2024-07-31 20:15:21 -0600

    yay

--- src/events/voice.ts
+++ src/events/voice.ts
@@ -6,22 +6,34 @@ export default createEvent({
     },
     run: async ([newState, oldState], client) => {
         if (oldState?.channelId === newState.channelId) return;
-        if (newState.channelId === null) return;
+        if (!newState.channelId) return;
 
         const { guildId } = newState;
+        
         const player = client.manager.getPlayer(guildId);
         if (!player) return;
 
         const stateChannel = await client.channels.fetch(player.voiceChannelId!);
-
         if (!stateChannel.is(["GuildVoice", "GuildStageVoice"])) return;
 
         // Check if voice channel is empty
         const members = await Promise.all((await stateChannel.states()).map((x) => x.member()));
         const isEmpty = members.filter((x) => !x.user.bot).length === 0;
 
+        if (
+            isEmpty &&
+            !player.playing &&
+            !player.paused &&
+            !(player.queue.tracks.length + Number(!!player.queue.current)) &&
+            player.connected
+        ) {
+            await player.destroy("Hinagi is... Sleeping.", true);
+            return;
+        }
+
         if (isEmpty) {
             await player.destroy("Empty voice channel.", true);
+            return;
         }
     },
 });
